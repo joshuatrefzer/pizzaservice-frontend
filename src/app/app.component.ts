d5c59ebf2fe4fbbb6e62cc946136b331d976e67e
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2025-01-17 15:35:33 +0100

    refactor: removed unused function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,13 +13,12 @@ import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet ,  HeaderComponent,  MatIconModule, MatButtonModule, MatBadgeModule, AccountDialogComponent , CommonModule, DialogComponent],
+  imports: [RouterOutlet ,  HeaderComponent,  MatIconModule, MatButtonModule, MatBadgeModule , CommonModule, DialogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   authenticationActive: WritableSignal<boolean> = signal(true);
-  popup: WritableSignal<boolean> = signal(false);
 
   title = 'pizzaservice';
 
@@ -44,11 +43,6 @@ export class AppComponent {
   navigateToCart(){
     this.router.navigate(['/shopping-cart']);
   }
-
-  togglePopup(event:any): void {
-    this.popup.set(event); 
-  }
-
 
 
 }
